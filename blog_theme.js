f40b7bef096ce0eae0a6e1b6f29761dd5fe1b184
Christian <chrisvergara7@outlook.com>
2023-04-23 22:37:01 -0600

    blog update

---
+++ blog_theme.js
@@ -0,0 +1,66 @@
+// Original file of Christian VR
+// Last modification = 11/04/2023
+// Copyright VR Cibersecurity 
+
+
+const toggleSwitch = document.querySelector('#toggle-theme');
+const body_object = document.querySelector('body');
+const navbar = document.querySelector('.navbar');
+const changeThemeButton = document.querySelector('.change-theme-button');
+const blogElements = [
+    '.super-container .category-col h4',
+    '.super-container .row',
+    '.super-container .block-blog',
+    '.super-container',
+]
+
+const containerBlogNavbarElements = [
+    '.containter-blog-navbar i',
+    '.containter-blog-navbar a'
+
+]
+
+toggleSwitch.addEventListener( 'change', () => {
+    changeBlogSvg('.main-svg .fil0');
+    changeBlogNavbar('.nav-link')
+    modifyBlogBlock(blogElements)
+    modifyContainerBlogNavbarElements(containerBlogNavbarElements)
+    changeThemeButton.classList.toggle('black');
+    body_object.classList.toggle('light-theme');
+    navbar.classList.toggle('light-theme');
+})
+
+function changeBlogSvg(object) {
+    var svgOBject = document.querySelectorAll(object);
+    svgOBject.forEach(element => {
+        element.classList.toggle('black-mode');
+    });
+}
+
+function changeBlogNavbar(object) {
+    var nav_link = document.querySelectorAll(object);
+    nav_link.forEach(element => {
+        element.classList.toggle('light-theme')
+    })
+}
+
+function modifyBlogBlock(object) {
+    for(var i = 0; i < object.length; i++) {
+        var element = document.querySelectorAll(object[i]);
+        element.forEach(item => {
+            item.classList.toggle('black');
+        });
+    }
+}
+
+function modifyContainerBlogNavbarElements(object) {
+    for(var i = 0; i < object.length; i++) {
+        var elements = document.querySelectorAll(object[i]);
+        elements.forEach(item => {
+            item.classList.toggle('black');
+        });
+    }
+}
+
+
+
